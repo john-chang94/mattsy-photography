982ba6395acbf56fc1da8a21493efcd9ac0e00c2
john-chang94 <39566708+john-chang94@users.noreply.github.com>
2019-04-09 15:42:41 -0700

    edit email form

--- send-email.php
+++ send-email.php
@@ -2,15 +2,19 @@
     $name = $_POST['name'];
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
-    $my_email = 'j.chang94@yahoo.com'
+    $my_email = 'changjohn94@gmail.com'
 ?>
 
 <?php
-    $to = $my_email;
-    $email_subject = "Contact form submission: $name";
-    $email_body = $message;
+    $toEmail = $my_email;
+    $subject = "Contact form submission: $name";
+    $body = $message;
+
+    $headers = "MIME-Version 1.0" ."\r\n";
+    $headers .= "Content-Type:text/html;charset=UTF-8" . "\r\n";
+    
     $headers = "From: $my_email\n";
     $headers .= "Reply-To: $visitor_email";
-    mail($to, $email_subject, $email_body, $headers);
+    mail($toEmail, $subject, $body, $headers);
     header("Location: contact.php?mailsend")
 ?>
