c1a1fe9e62d5be3fe66aaa0171625753f93bbe96
john-chang94 <39566708+john-chang94@users.noreply.github.com>
2019-04-09 12:16:12 -0700

    added php email function

---
+++ send-email.php
@@ -0,0 +1,15 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+    $my_email = 'changjohn94@gmail.com'
+?>
+
+<?php
+    $to = $my_email;
+    $email_subject = "Contact form submission: $name";
+    $email_body = $message;
+    $headers = "From: $my_email\n";
+    $headers .= "Reply-To: $visitor_email";
+    mail($to, $email_subject, $email_body, $headers);
+?>
